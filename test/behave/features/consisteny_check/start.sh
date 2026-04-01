017661f3879088513a8885d5dd8bde19f36912c6
Simon Bader <bader@semafor.ch>
2026-04-01 15:16:56 +0200

    behave test consistency_check: fix trap signals (SIGTERM > TERM, SIGINT > INT) for dash

--- test/behave/features/consisteny_check/start.sh
+++ test/behave/features/consisteny_check/start.sh
@@ -10,7 +10,7 @@ export INTENS_REPLY_B_PORT=${2:-24560}
 # start api gateway
 python mqrep.py &
 API_PID=$!
-trap "kill -9 ${API_PID}" SIGTERM SIGINT EXIT
+trap "kill -9 ${API_PID}" TERM INT EXIT
 
 # consistency check
 intens --replyPort ${INTENS_REPLY_PORT} etc/consistency_check.des

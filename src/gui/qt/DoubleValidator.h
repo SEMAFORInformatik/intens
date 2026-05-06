4a5fff65c17d68e32d80aa6a4b1c177264faa38b
Guenther Amsler <amsler@semafor.ch>
2026-05-06 08:43:37 +0200

    #28 add files

---
+++ src/gui/qt/DoubleValidator.h
@@ -0,0 +1,33 @@
+#if !defined(GUI_QT_DOUBLEVALIDATOR_INCLUDED_H)
+#define GUI_QT_DOUBLEVALIDATOR_INCLUDED_H
+
+#include <QDoubleValidator>
+
+class QWidget;
+class QString;
+class Scale;
+
+class DoubleValidator : public QDoubleValidator {
+public:
+  DoubleValidator( QObject * parent, Scale* scale=0 );
+  DoubleValidator( double bottom, double top, int decimals,
+		    QObject * parent );
+  virtual ~DoubleValidator();
+
+  virtual QValidator::State validate( QString & input, int & pos ) const;
+  virtual void setRange ( double bottom, double top );
+  bool acceptLocalizedNumbers() const;
+  void setAcceptLocalizedNumbers( bool accept );
+  static QString decimalSymbol();
+  static QString thousandsSeparator();
+
+
+ private:
+  typedef QDoubleValidator base;
+  class Private;
+  Scale*    m_scale;
+  double _min;
+  double _max;
+};
+
+#endif
